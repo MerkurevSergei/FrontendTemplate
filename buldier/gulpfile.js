732454b5e75ef08ac05ebd374c153a01164977eb
SergMers <Mininurka@yandex.ru>
2017-06-27 00:10:36 +0900

    Рефракторинг и создание задач gulp

--- buldier/gulpfile.js
+++ buldier/gulpfile.js
@@ -1,1 +1,14 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+	
+    gulp.src('./../source/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./../source/css/'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./../source/sass/**/*.scss', ['sass']);
+});
+
